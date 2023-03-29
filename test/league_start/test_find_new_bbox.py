34c95afca15ad99aac50a0d8b844798a1d706c1b
Felix <Felix.Wedlich@gmail.com>
2023-03-29 16:42:14 +0200

    add mana test

--- test/league_start/test_find_new_bbox.py
+++ test/league_start/test_find_new_bbox.py
@@ -25,11 +25,29 @@ class TestFindNewBBOX(unittest.TestCase):
                     detector.bbox = new_bbox
                     val = detector.detect(sct,False)
                     if val:
-                        print(f"new bbox found {new_bbox}, val: {val}")
+                        print(f"LIFE: new bbox found {new_bbox}, val: {val}")
+                        return
 
         pass
     def test_mana(self):
-        pass
+        shared_mem = SharedMemory("MANA", create=True, size=11)
+        OLD_BBOX = (2285, 1119, 2513, 1146)
+        detector = Detector(OLD_BBOX,shared_mem)
+        detector._init_attrs()
+        #first test  identical bbox sizes:
+        OLD_HEIGHT = OLD_BBOX[3] - OLD_BBOX[1]
+        OLD_WIDTH = OLD_BBOX[2] - OLD_BBOX[0]
+
+        with mss.mss() as sct:
+            # for x in range(-30,30):
+            for x in range(-10,10):
+                for y in range(-50,0):
+                    new_bbox = (OLD_BBOX[0]+x,OLD_BBOX[1]+y,OLD_BBOX[2]+x,OLD_BBOX[3]+y)
+                    detector.bbox = new_bbox
+                    val = detector.detect(sct,False)
+                    if val:
+                        print(f"MANA: new bbox found {new_bbox}, val: {val}")
+                        return
 
 
 
